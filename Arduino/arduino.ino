f58d2bd5585bd9a5e75a7a321c1730e85fb3f917
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-04-15 13:03:32 -0700

    Better command consumption from serial

--- Arduino/arduino.ino
+++ Arduino/arduino.ino
@@ -1,47 +1,64 @@
-unsigned long startTime, currentTime, instructionEnd;
-char currentInstruction = '\0';
+typedef struct {
+    char actionCode;
+    unsigned int actionEnd;
+} Instruction;
+Instruction STOP = {'\0', 0};
+Instruction currentInstruction = STOP;
+unsigned long startTime, currentTime;
+const unsigned int instructionSize = sizeof(char) + sizeof(unsigned int);
 
 void setup() {
     Serial.begin(9600);
+    Serial1.begin(9600);
     Serial.println("Starting");
 
     pinMode(LED_BUILTIN, OUTPUT);
 
     startTime = millis();
-    instructionEnd = 0;
 }
 
 void continueAction() {
-    Serial.println("Action Performed");
+    if (currentInstruction.actionCode == STOP.actionCode) {
+        return;
+    }
+    Serial.print("Action Performed: ");
+    Serial.println(currentInstruction.actionCode);
 }
 
 void readAction() {
-    if (Serial.available() > 0) {
-        char readByte = Serial.read();
-        if (readByte != -1) {
-            if (currentInstruction == '\0') {
-                Serial.println("Nothing read and nothing valid to resume");
-            } else {
-                Serial.println("Unable to read. Continuing expired instruction");
-            }
-        } else {
-            currentInstruction = readByte;
-        }
-    } else {
-        Serial.println("Serial Unavailable");
+    // TODO: Wire Serial1 TX and RX pins
+    // if (Serial1.available() < instructionSize) {
+    if (Serial.available() < instructionSize) {
+        Serial.println("Serial Empty/Unavailable");
+        currentInstruction = STOP;
+        return;
     }
+
+    // TODO: Read more than just the actionCode
+    // TODO: Wire Serial1 TX and RX pins
+    // char readByte = Serial1.read();
+    char readByte = Serial.read();
+    if (readByte == -1) {
+        Serial.println("Unable to read. Stopping");
+        currentInstruction = STOP;
+        return;
+    }
+
+    Serial.print("Read: ");
+    Serial.println(readByte);
+    currentInstruction = {readByte, 0};
 }
 
 void loop() {
     currentTime = millis();
     const unsigned long elapsedTime = currentTime - startTime;
-    if (elapsedTime >= instructionEnd) {
+    if (elapsedTime >= currentInstruction.actionEnd) {
         readAction();
     }
     continueAction();
 
-    delay(1000);
+    delay(500);
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    delay(500);
     digitalWrite(LED_BUILTIN, LOW);
 }
