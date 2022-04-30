1885bd09815028344b7eb7deb02c78066df9619c
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-04-30 16:08:15 -0700

    Switched motor controlling code to digital. Relays should not be PWM'd

--- Arduino/commands.cpp
+++ Arduino/commands.cpp
@@ -11,10 +11,12 @@ void setLED(bool turnOn) {
 
 void setMotorLeft(float percentage) {
 	// Pin 5 PWM
-	analogWrite(5, (int) (percentage * 255));
+	// analogWrite(5, (int) (percentage * 255));
+	digitalWrite(2, percentage < 0.001f ? LOW : HIGH);
 }
 
 void setMotorRight(float percentage) {
 	// Pin 6 PWM
-	analogWrite(6, (int) (percentage * 255));
+	// analogWrite(6, (int) (percentage * 255));
+	digitalWrite(3, percentage < 0.001f ? LOW : HIGH);
 }
